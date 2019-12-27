1949ebc0f5ea5e705c9f7c50ad2797f62a0d69a1
Phuc Tran Truong <ptruong@outlook.de>
2019-12-27 17:32:22 +0100

    Add SpanUtility class

--- tlbiore/data/utils.py
+++ tlbiore/data/utils.py
@@ -1,6 +1,38 @@
-import pandas as pd
 from typing import List
+from tlbiore.data.corpus import Span
 from sklearn.model_selection import train_test_split
+
+
+class SpanUtils:
+    @staticmethod
+    def get_spans(char_offset):
+        spans = []
+        for span in char_offset.split(','):
+            limits = span.split('-')
+            start = int(limits[0])
+            end = int(limits[1]) + 1
+            spans.append(Span(start, end))
+        return spans
+
+    @staticmethod
+    def equals(span_a: Span, span_b: Span):
+        return span_a.start == span_b.start and span_a.end == span_b.end
+
+    @staticmethod
+    def contains(span_a: Span, span_b: Span):
+        return span_a.start <= span_b.start and span_a.end >= span_b.end
+
+    @staticmethod
+    def intersects(span_a: Span, span_b: Span):
+        return span_a.start < span_b.end and span_a.end > span_b.start
+
+    @staticmethod
+    def intersects_any(spans_a: List[Span], spans_b: List[Span]):
+        for span_a in spans_a:
+            for span_b in spans_b:
+                if SpanUtils.intersects(span_a, span_b):
+                    return True     # TODO or return all the intersecting span pairs?
+        return False
 
 
 def get_span(entity_no, spans):
@@ -31,8 +63,9 @@ def split_sentence(span_list, sentence, include_entities=False):
 def train_dev_test_split(df, split_ratio=(0.8, 0.1, 0.1)):
     train_size, dev_size, test_size = split_ratio
     # TODO split at sentence level
+    # TODO using stratify option requires separate label data frame
     train, tmp = train_test_split(df, random_state=2018, train_size=train_size)
-    dev, test = train_test_split(tmp, random_state=2018, test_size=test_size/(test_size+dev_size))
+    dev, test = train_test_split(tmp, random_state=2018, test_size=test_size / (test_size + dev_size))
     return train, dev, test
 
 
